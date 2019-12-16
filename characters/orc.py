97e1f2421f76d220cf43efc5ea6dd843ace3c046
fberrabah <farid.berrabah@gmail.com>
2019-12-16 16:25:38 +0100

    create class

---
+++ characters/orc.py
@@ -0,0 +1,7 @@
+from person import Person
+
+class Orc(Person):
+
+    def __init__(self):
+    self.name = name 
+    super().__init__(life, attack, defence, agility)
---
+++ characters/zombie.py
@@ -0,0 +1,7 @@
+from person import Person
+
+class Zombie(Person):
+
+    def __init__(self):
+    self.name = name 
+    super().__init__(life, attack, defence, agility)
