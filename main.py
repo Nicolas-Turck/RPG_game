48f52d04348666ed0f4d8e06896bb559acfb3360
User <user@Users-MacBook-Pro.local>
2019-12-17 11:14:51 +0100

    change main import

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from characters.character import Person
 from characters.magicien import Magicien
 from characters.archer import Archer
+from characters.guerrier import Guerrier
 if __name__=='__main__':
 
 
