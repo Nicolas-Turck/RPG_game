f628687247258e675e8edb40db5389e133599bce
User <user@Users-MacBook-Pro.local>
2019-12-17 11:30:21 +0100

    change main

--- main.py
+++ main.py
@@ -7,16 +7,4 @@ if __name__=='__main__':
 
 
 
-    names = enter_name()
-
-    mag = Magicien("nicos")
-    arc = Archer("farid")
-    print(mag.__dict__)
-    print(arc.__dict__)
-    #narration
-    #start game
-    #random choice ennemiss
-    #display game choice
-    #choose choice attack or stop combat
-    #program play
-    #display result
+   
