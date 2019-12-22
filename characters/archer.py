0f33ebe03bd0eb889bfe09c2d594477125173247
Turck Nicolas <56393359+Nicolas-Turck@users.noreply.github.com>
2019-12-22 19:00:41 +0100

    Update archer.py
    changes life

--- characters/archer.py
+++ characters/archer.py
@@ -5,7 +5,7 @@ class Archer(Person):
     """class for archer player"""
     def __init__(self, name):
         self.name = name
-        super().__init__(350, 80, 30, 50, name)
+        super().__init__(1, 80, 30, 50, name)
 
     def heal(self, player):
         self.player = player
